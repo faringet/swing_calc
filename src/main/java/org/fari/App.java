982e733e568244bb10fb86b571120a8743e64b4d
faringet <mik.davydov14@gmail.com>
2022-09-09 00:55:57 +0300

    added swing_calc

---
+++ src/main/java/org/fari/App.java
@@ -0,0 +1,9 @@
+package org.fari;
+
+
+public class App 
+{
+    public static void main( String[] args ) {
+        new JFrameApp();
+    }
+}
---
+++ src/main/java/org/fari/JFrameApp.java
@@ -0,0 +1,16 @@
+package org.fari;
+
+import javax.swing.*;
+
+public class JFrameApp extends JFrame {
+
+    public JFrameApp() {
+        setBounds(100,100,265,400);
+        setTitle("Calculator");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        add(new JPanelApp());
+        setVisible(true);
+
+
+    }
+}
